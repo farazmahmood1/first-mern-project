c11f8d60f00ae6adea7ef8591f9b1e85729e9218
Faraz Mahmood <faraz.m4765@gmail.com>
2023-08-10 17:56:53 +0500

    first commit

---
+++ api/route/student.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const Student = require("../model/student");
+const mongoose = require("mongoose");
+
+// api of get request
+router.get("/", (req, res, next) => {
+  // res.status(200).json({
+  //   message: "Welcome to the API student get request",
+  // });
+
+  Student.find()
+  .then(result => {
+    res.status(200).json({
+      studentData:result
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({
+      error:err
+    })
+  })
+
+});
+
+
+// post api
+router.post("/", (req, res, next) => {
+
+  const student = new Student({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    gender: req.body.gender,
+  });
+
+
+  student.save()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        newStudent: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+
+
+
+// importing the routes
+const studentRoute = require("./api/route/student");
+const facultyRoute = require("./api/route/faculty");
+
+// 
+const bodyParser = require('body-parser')
+
+
+
+// importing mongodb
+const mongoose = require("mongoose");
+// password connection  database ==> connect ===> drivers
+mongoose.connect(
+  "mongodb+srv://faraz:11farazmahmood11@cluster0.lpdtumq.mongodb.net/?retryWrites=true&w=majority"
+);
+// checking the connection of the mongodb
+mongoose.connection.on("error", (err) => {
+  console.log("connection failed");
+});
+mongoose.connection.on("connected", (connected) => {
+  console.log("succesfull");
+});
+
+
+// bodyparsing usage
+app.use(bodyParser.urlencoded({extended:'false'}));
+app.use(bodyParser.json());
+
+
+//assigning the name for the apis
+app.use("/student", studentRoute);
+app.use("/faculty", facultyRoute);
+
+// setting the route of the invalid path
+app.use((req, res, next) => {
+  res.status(400).json({
+    error: "invalid route",
+  });
+});
+
+// setting the response of the app
+
+// app.use((req, res, next) => {
+//   res.status(200).json({
+//     message: "app is running",
+//   });
+// });
+
+module.exports = app;
