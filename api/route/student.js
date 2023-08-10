b76c67edabee325c2b9c8247dbb0039614e6e448
Faraz Mahmood <faraz.m4765@gmail.com>
2023-08-10 23:17:35 +0500

    working on the registeration

--- api/route/student.js
+++ api/route/student.js
@@ -10,24 +10,66 @@ router.get("/", (req, res, next) => {
   // });
 
   Student.find()
-  .then(result => {
-    res.status(200).json({
-      studentData:result
+    .then((result) => {
+      res.status(200).json({
+        studentData: result,
+      });
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(500).json({
-      error:err
-    })
-  })
-
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
+// get api with user ID
+router.get("/:id", (req, res, next) => {
+  console.log(req.params.id);
+  Student.findById(req.params.id)
+    .then((result) => {
+      res.status(200).json({
+        student: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+// update using put request
+
+router.put("/:id", (req, res, next) => {
+  console.log(req.params.id);
+  Student.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        gender: req.body.gender,
+      },
+    }
+  )
+    .then((result) => {
+      res.status(200).json({
+        updated: result,
+      });
+    })
+    .catch((Err) => {
+      console.log(Err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
 // post api
 router.post("/", (req, res, next) => {
-
   const student = new Student({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
@@ -36,13 +78,53 @@ router.post("/", (req, res, next) => {
     gender: req.body.gender,
   });
 
-
-  student.save()
+  student
+    .save()
     .then((result) => {
       console.log(result);
       res.status(200).json({
         newStudent: result,
       });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+// delete api with userid
+// router.delete('/:id', (req,res,next) => {
+//   Student.remove({_id:req.params.id})
+//   .then(result=> {
+//     res.status(200).json({
+//       message:'id deleted',
+//       result:result
+//     })
+//   })
+//   .catch(err => {
+//     res.status(500).json({
+//       err:err
+//     })
+//   })
+// })
+
+router.delete("/:id", (req, res, next) => {
+  const studentId = req.params.id;
+
+  Student.findByIdAndRemove(studentId)
+    .then((result) => {
+      if (result) {
+        res.status(200).json({
+          message: "Student deleted successfully",
+          deletedStudent: result,
+        });
+      } else {
+        res.status(404).json({
+          message: "Student not found",
+        });
+      }
     })
     .catch((err) => {
       console.log(err);
